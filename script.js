e8e78500d5dd8f77dc3487cde13cc1af3d7b243e
Luis Vinicius <camperqueodeiacamper@gmail.com>
2022-02-18 05:10:35 -0300

    adjust images

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 icons = [
   {'link':'https://media.discordapp.net/attachments/916375412949528618/944127895557795860/bc227b46acba4458a9eca0ef73d95d50.jpg', 'tag':'GlitchCore'},
-  {'link':'https://cdn.discordapp.com/attachments/916375412949528618/944132948976865381/wp10229075.jpg', 'tag':'Gothic'}
+  {'link':'https://cdn.discordapp.com/attachments/916375412949528618/944132948976865381/wp10229075.jpg', 'tag':'Gothic'},
+  {'link': 'https://cdn.discordapp.com/attachments/916375412949528618/944135905168474142/wp10229051-dark-anime-icons-wallpapers.jpg', 'tag':'Gothic'},
+  {'link':'https://cdn.discordapp.com/attachments/916375412949528618/944137429613416498/wp10229060-dark-anime-icons-wallpapers.jpg', 'tag':'Gothic'},
+  {'link':'https://cdn.discordapp.com/attachments/916375412949528618/944138342226858045/acf6e0e16ecab2d9dce4f80a2a5d5435.jpg', 'tag':'Gothic'},
+  {'link':'https://cdn.discordapp.com/attachments/916375412949528618/944138597659975680/62f1e7855ec72228b9943dc5c865a5f3.jpg', 'tag':'Cute'},
+  {'link':'https://cdn.discordapp.com/attachments/916375412949528618/944139067803721728/240836407288fe134e9591f627087258.jpg', 'tag':'Cute'},
+  {'link':'https://cdn.discordapp.com/attachments/916375412949528618/944139525230305310/original.jpg', 'tag':'Cute'},
+  {'link':'https://cdn.discordapp.com/attachments/916375412949528618/944140262232440843/d662395813e4bfb309e1598b89f89291.jpg', 'tag':'GlitchCore'},
+  {'link':'https://cdn.discordapp.com/attachments/916375412949528618/944140502972919828/3e373ecc3d55a6615e52aee81327c0af.jpg', 'tag':'GlitchCore'},
+  {'link':'https://cdn.discordapp.com/attachments/916375412949528618/944140977176731698/70860521a2d611477f60a3c11b070ed3.jpg', 'tag':'GlitchCore'},
+  // {'link':'', 'tag':''},
+  // {'link':'', 'tag':''},
+  // {'link':'', 'tag':''},
+
+
   ]
 
 function icons1() {
@@ -13,4 +27,38 @@ function icons1() {
       document.getElementsByClassName("images")[0].innerHTML = aux;
     })}
 icons1()
-function GlitchCore()
+
+function GlitchCore() {
+  let aux = ""
+    icons.forEach(value => {
+      if (value.tag == 'GlitchCore'){
+        aux += `
+      <li class="imgitem">
+        <img class="imglink" src="${value.link}">
+      </li>`
+      }
+    document.getElementsByClassName("images")[0].innerHTML = aux;
+    })}
+
+function Gothic() {
+  let aux = ""
+    icons.forEach(value => {
+      if (value.tag == 'Gothic'){
+        aux += `
+      <li class="imgitem">
+        <img class="imglink" src="${value.link}">
+      </li>`
+      }
+    document.getElementsByClassName("images")[0].innerHTML = aux;
+    })}
+function Cute() {
+  let aux = ""
+    icons.forEach(value => {
+      if (value.tag == 'Cute'){
+        aux += `
+      <li class="imgitem">
+        <img class="imglink" src="${value.link}">
+      </li>`
+      }
+    document.getElementsByClassName("images")[0].innerHTML = aux;
+    })}
